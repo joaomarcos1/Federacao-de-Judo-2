c1ed3348e4f3d7903200ee56f732e4650ab848e2
brenamaia <brenamaia12@gmail.com>
2017-11-29 19:30:36 -0300

    commit

--- judo/settings.py
+++ judo/settings.py
@@ -75,11 +75,24 @@ WSGI_APPLICATION = 'judo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+'''
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'judo',
+		'USER': 'root',
+		'PASSWORD': 'root',
+		'HOST': 'localhost',
+		'PORT': '3306',
+	}
 }
 
 
