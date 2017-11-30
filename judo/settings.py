fd0be45822bc31812622c57a83e87c392480f3c1
brenamaia <brenamaia12@gmail.com>
2017-11-30 12:03:45 -0300

    index

--- judo/settings.py
+++ judo/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'judo',
 		'USER': 'root',
-		'PASSWORD': 'admin',
+		'PASSWORD': 'root',
 		'HOST': 'localhost',
 		'PORT': '3306',
 	}
